59582d565e617976a1820ad84d7c3ad5400e8882
Roy C Wright <roy.wright@mobileappscompany.us>
2020-02-24 13:10:17 -0500

    Successfully tracking button clicks and storing them in an array

--- index.js
+++ index.js
@@ -6,12 +6,44 @@ let yellow = new Audio("../Simon/public/sounds/yellow.mp3");
 let red = new Audio("../Simon/public/sounds/red.mp3");
 let green = new Audio("../Simon/public/sounds/green.mp3");
 const playBtns = [green, red, yellow, blue];
+let gamePattern = [];
+let playerPattern = [];
 function getRandomNum(){
   return Math.floor((Math.random() * 4) + 1);
 }
 //console.log(getRandomNum());
+
+function greenPlay(){
+  green.play();
+  game[1].classList.add("selected");
+  setTimeout(()=>{game[1].classList.remove("selected");}, 500);
+  playerPattern.push(game[1].name);
+  console.log(playerPattern);
+}
+function redPlay(){
+  red.play();
+  game[2].classList.add("selected");
+  setTimeout(()=>{game[2].classList.remove("selected");}, 500);
+  playerPattern.push(game[2].name);
+  console.log(playerPattern);
+}
+function yellowPlay(){
+  yellow.play();
+  game[3].classList.add("selected");
+  setTimeout(()=>{game[3].classList.remove("selected");}, 500);
+  playerPattern.push(game[3].name);
+  console.log(playerPattern);
+}
+function bluePlay(){
+  blue.play();
+  game[4].classList.add("selected");
+  setTimeout(()=>{game[4].classList.remove("selected");}, 500);
+  playerPattern.push(game[4].name);
+  console.log(playerPattern);
+}
+
 function startGame(){
-  game[0].classList.add("hide");
+
   let val = getRandomNum(); //first number selected
 
     game[val].classList.add("selected");//change to a temp flash
@@ -21,125 +53,40 @@ function startGame(){
     game[val].classList.remove("selected");
   }, 500); //remove classList
 
-  let isEqual = true;
-  // while(isEqual){
-  //
-  //
-  // }
+    gamePattern.push(game[val]);
+    //console.log(game[val].name);
 
 }
-function timeout(){ setTimeout(startGame, 500 );}
+function timeout(){
+  game[0].classList.add("hide");
+  setTimeout(startGame, 500 );
+}
+
 function alerting(){
     blue.play();
     game[1].classList.remove("btn1");
     game[1].classList.add("btnChange");
   }
-function logging(){console.log("Naw son!");}
+
 for(let i = 0; i<game.length; i++){
 
   switch(i){
     case 0:
       game[i].addEventListener("click", timeout);
       break;
-    case 1:
-      game[i].addEventListener("click", alerting);
+    case 1: //green
+      game[i].addEventListener("click", greenPlay);
       break;
-    case 2:
-      game[i].addEventListener("click", alerting);
+    case 2: //red
+      game[i].addEventListener("click", redPlay);
       break;
-    case 3:
-      game[i].addEventListener("click", alerting);
+    case 3: //yellow
+      game[i].addEventListener("click", yellowPlay);
       break;
-    case 4:
-      game[i].addEventListener("click", alerting);
+    case 4: //blue
+      game[i].addEventListener("click", bluePlay);
       break;
     default:
     console.log("This ain't it bud.");
   }
 }
-
-// game.forEach(btn =>{
-//   btn.addEventListener("click", ()=>{
-//     errorSound.play();
-//     alert("I got clicked");
-//     //console.log(btn.indexOf());
-//   });
-// });
-
-// let start = document.querySelectorAll("button")[0];
-// let green = document.querySelectorAll("button")[1];
-//
-// let red = document.querySelectorAll("button")[2];
-// let yellow = document.querySelectorAll("button")[3];
-// let blue = document.querySelectorAll("button")[4];
-
-//console.log(btns);
-// let greenSound = new Audio("../Simon/public/sounds/green.mp3");
-// let redSound = new Audio("../Simon/public/sounds/red.mp3");
-// let yellowSound = new Audio("../Simon/public/sounds/yellow.mp3");
-// let blueSound = new Audio("../Simon/public/sounds/blue.mp3");
-// let errorSound = new Audio("../Simon/public/sounds/wrong.mp3");
-// let gameStarted = false;
-// const list = [];
-// let inc = 0;
-//
-// function randomGenerator() {
-//   random = Math.floor(Math.random() * 4) + 1;
-//   return random;
-// }
-// let random = randomGenerator();
-//
-// //console.log(btns[random].setAttribute("style", "background-color: magenta"));
-// function greenBtn(){
-//   greenSound.play();
-//   inc++;
-//   document.getElementById("heading1").firstElementChild.innerText = `Round ${inc}`;
-//   //green.classList.add();
-// }
-// function redBtn(){
-//   redSound.play();
-//   inc++;
-//   document.getElementById("heading1").firstElementChild.innerText = `Round ${inc}`;
-// }
-// function yellowBtn(){
-//    yellowSound.play();
-//    inc++;
-//    document.getElementById("heading1").firstElementChild.innerText = `Round ${inc}`;
-//  }
-// function blueBtn(){
-//   blueSound.play();
-//   inc++;
-//   document.getElementById("heading1").firstElementChild.innerText = `Round ${inc}`;
-// }
-// function wrong(){
-//   errorSound.play();
-//
-//   document.getElementById("heading1").firstElementChild.innerText = "Game Over";
-// }
-//
-// function startGame(){
-//   inc++;
-//   start.style.visibility = "hidden";
-//   document.getElementById("heading1").firstElementChild.innerText = `Round 1`;
-//
-//   isMatched = true;
-// }
-//
-// green.addEventListener("click", greenBtn);
-// red.addEventListener("click", redBtn);
-// yellow.addEventListener("click", yellowBtn);
-// blue.addEventListener("click", ()=>{
-//
-//   blueSound.play();
-// });
-// start.addEventListener("click", startGame);
-// //start while loop
-// random = randomGenerator();
-// console.log(random);
-//
-
-
-// if(){
-//
-// }
-//End the while loop
