2dda635983b5682388ef74b57a510846ffb3dfae
Roy C Wright <roy.wright@mobileappscompany.us>
2020-02-24 20:23:47 -0500

    Most of the functionality works, must change banner and set delay on next trigger.

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ let blue = new Audio("../Simon/public/sounds/blue.mp3");
 let yellow = new Audio("../Simon/public/sounds/yellow.mp3");
 let red = new Audio("../Simon/public/sounds/red.mp3");
 let green = new Audio("../Simon/public/sounds/green.mp3");
+let wrong = new Audio("../Simon/public/sounds/wrong.mp3");
+let inc = 0;
+let patternMatch = true;
 const playBtns = [green, red, yellow, blue];
+let btnValues = [];
 let gamePattern = [];
 let playerPattern = [];
 function getRandomNum(){
   return Math.floor((Math.random() * 4) + 1);
 }
-//console.log(getRandomNum());
+function checkAll(){
+  //create a checkAll method!
+    for(var i=0; i<playerPattern.length; i++){
+      if(gamePattern[i] !== playerPattern[i]){
+        document.querySelector("html").classList.add("selected");
+        wrong.play();
+        gamePattern = [];
+        playerPattern = [];
+        game[0].classList.remove("hide");
+      }
+    }//After it checks all and they match, empty playerPattern play again
+
+    playGame();
+
+}
 
 function greenPlay(){
   green.play();
@@ -19,6 +37,24 @@ function greenPlay(){
   setTimeout(()=>{game[1].classList.remove("selected");}, 500);
   playerPattern.push(game[1].name);
   console.log(playerPattern);
+  console.log(gamePattern);
+
+  /*button pressed: create separate fun, pass to each play function
+  -while with boolean match checker
+  -check if pattern is equal for/foreach loop
+  -reset game once out of for loop
+  -the prompt button only triggers once per round
+  --gamePattern = [red, green]
+  */
+  checkAll();
+
+
+
+  // if(playerPattern[inc] === gamePattern[inc]){
+  //   alert("You got it son!");
+  // }else{
+  //   alert("But you're wrong...");
+  // }
 }
 function redPlay(){
   red.play();
@@ -26,6 +62,14 @@ function redPlay(){
   setTimeout(()=>{game[2].classList.remove("selected");}, 500);
   playerPattern.push(game[2].name);
   console.log(playerPattern);
+  console.log(gamePattern);
+  checkAll();
+  // if(playerPattern[inc] === gamePattern[inc]){
+  //   startGame();
+  //   inc++;
+  // }else{
+  //     wrong.play();
+  // }
 }
 function yellowPlay(){
   yellow.play();
@@ -33,6 +77,13 @@ function yellowPlay(){
   setTimeout(()=>{game[3].classList.remove("selected");}, 500);
   playerPattern.push(game[3].name);
   console.log(playerPattern);
+  console.log(gamePattern);
+  checkAll();
+  // if(playerPattern[inc] === gamePattern[inc]){
+  //   alert("You got it son!");
+  // }else{
+  //   alert("But you're wrong...");
+  // }
 }
 function bluePlay(){
   blue.play();
@@ -40,39 +91,40 @@ function bluePlay(){
   setTimeout(()=>{game[4].classList.remove("selected");}, 500);
   playerPattern.push(game[4].name);
   console.log(playerPattern);
+  console.log(gamePattern);
+
+  checkAll();
+  // if(playerPattern[inc] === gamePattern[inc]){
+  //   alert("You got it son!");
+  // }else{
+  //   alert("But you're wrong...");
+  // }
 }
 
-function startGame(){
-
-  let val = getRandomNum(); //first number selected
-
+function playGame(){
+//call this everytime
+    let val = getRandomNum(); //first number selected
+    btnValues.push(val);
     game[val].classList.add("selected");//change to a temp flash
     playBtns[val-1].play();
-  console.log(game[val].name);
-  setTimeout(()=>{
-    game[val].classList.remove("selected");
-  }, 500); //remove classList
+    console.log(game[val].name);
+    setTimeout(()=>{
+      game[val].classList.remove("selected");
+    }, 500); //remove classList
 
-    gamePattern.push(game[val]);
-    //console.log(game[val].name);
-
+    gamePattern.push(game[val].name);
 }
-function timeout(){
+function startGame(){//start game button triggers this function
   game[0].classList.add("hide");
-  setTimeout(startGame, 500 );
+  setTimeout(playGame, 500 );
 }
 
-function alerting(){
-    blue.play();
-    game[1].classList.remove("btn1");
-    game[1].classList.add("btnChange");
-  }
 
 for(let i = 0; i<game.length; i++){
 
   switch(i){
     case 0:
-      game[i].addEventListener("click", timeout);
+      game[i].addEventListener("click", startGame);
       break;
     case 1: //green
       game[i].addEventListener("click", greenPlay);
